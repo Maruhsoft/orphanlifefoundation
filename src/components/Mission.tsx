d609c6231fe3e3feddda2351bbc38e7d94d7ea1c
Maruh Phronesis Vulcan Akporowho <59612588+Maruhsoft@users.noreply.github.com>
2025-01-24 03:34:05 -0800

    Update Mission.tsx

--- src/components/Mission.tsx
+++ src/components/Mission.tsx
@@ -47,7 +47,7 @@ const Mission = () => {
           <div className="mt-10 lg:mt-0">
             <div className="aspect-w-16 aspect-h-9">
               <img
-                className="rounded-lg shadow-lg object-cover"
+                className="shadow-lg object-cover"
                 src="https://images.unsplash.com/photo-1617878227827-8360231f7f03?q=80&w=1556&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
                 alt="Children Eating"
               />
