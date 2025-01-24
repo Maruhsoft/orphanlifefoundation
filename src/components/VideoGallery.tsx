c3b29e55a7324bf493414608b0cb848ad860c5b8
Maruh Phronesis Vulcan Akporowho <59612588+Maruhsoft@users.noreply.github.com>
2025-01-24 03:10:28 -0800

    Update VideoGallery.tsx

--- src/components/VideoGallery.tsx
+++ src/components/VideoGallery.tsx
@@ -6,7 +6,7 @@ const VideoGallery = () => {
     {
       id: 1,
       title: 'Making a Difference',
-      thumbnail: 'https://images.unsplash.com/photo-1488521787991-ed7bbaae773c?ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=80',
+      thumbnail: 'https://plus.unsplash.com/premium_photo-1683121334505-907a00cf904c?q=80&w=1632&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
       duration: '2:45',
     },
     {
